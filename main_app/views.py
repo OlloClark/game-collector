59283565ce329c69581b7eb3425f6b4b2365cd69
OlloClark <ollo42@hotmail.co.uk>
2022-04-25 15:56:12 -0700

    first lesson plan complete w fake model

--- main_app/views.py
+++ main_app/views.py
@@ -4,9 +4,27 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+# TEMP DATA
+class Game:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, publisher, players, theme):
+    self.name = name
+    self.publisher = publisher
+    self.players = players
+    self.theme = theme
+	
+games = [
+  	Game("Snap", "card games", 2, "easy time-passing"),
+  	Game("snakes n ladders", "hasbro", 2, "mindless chance"),
+]
+
 # Define the home view
 def home(request):
 	return HttpResponse('<h1>Game Collector Home Page</h1>')
 
 def about(request):
 	return render(request, "about.html")
+
+def games_index(request):
+	return render(request, "games/index.html", {"games": games})
+
+	
